76f004d7ae33c1bd7395cbaa2bd6fbd298e11f72
jungchul <skdlxp17@naver.com>
2014-12-05 10:00:59 +0900

    커밋

--- src/feelsu/controller/FeelsuController.java
+++ src/feelsu/controller/FeelsuController.java
@@ -2,14 +2,22 @@ package feelsu.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import dao.FeelsuDao;
 @Controller
 public class  FeelsuController{
-	
+	private FeelsuDao feelsuDao;
 	@RequestMapping("feelsu.do")
 	public String form() {
 		
 		return "/feelsu/feelsu.jsp";
 		
 	}
+	public FeelsuDao getFeelsuDao() {
+		return feelsuDao;
+	}
+	public void setFeelsuDao(FeelsuDao feelsuDao) {
+		this.feelsuDao = feelsuDao;
+	}
+	
+	
 }
